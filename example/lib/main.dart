97c329dd475c0d5fd6a2794e0e865a5feb9af219
moluopro <2056673609@qq.com>
2023-07-24 15:52:10 +0800

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 class MyBrowser extends StatefulWidget {
   const MyBrowser({Key? key, this.title}) : super(key: key);
   final String? title;
-  
+
   @override
   MyBrowserState createState() => MyBrowserState();
 }
