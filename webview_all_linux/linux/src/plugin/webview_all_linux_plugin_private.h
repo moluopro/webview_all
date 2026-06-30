83ed278ca7bc172f7e13c7d9002e8a66640ff2e9
moluopro <moluopro@qq.com>
2026-06-30 12:45:34 +0800

    feat(linux): expand plugin private API

--- webview_all_linux/linux/src/plugin/webview_all_linux_plugin_private.h
+++ webview_all_linux/linux/src/plugin/webview_all_linux_plugin_private.h
@@ -10,31 +10,37 @@
 struct _WebviewAllLinuxPlugin {
   GObject parent_instance;
 
-  FlPluginRegistrar* registrar;
-  FlMethodChannel* root_channel;
-  GtkOverlay* overlay;
-  GHashTable* webviews;
+  FlPluginRegistrar *registrar;
+  FlMethodChannel *root_channel;
+  GtkOverlay *overlay;
+  GHashTable *webviews;
   gint next_webview_id;
 };
 
 typedef struct {
-  WebviewAllLinuxPlugin* plugin;
+  WebviewAllLinuxPlugin *plugin;
   gint id;
-  WebKitUserContentManager* content_manager;
-  WebKitWebView* web_view;
-  FlMethodChannel* method_channel;
-  FlEventChannel* event_channel;
+  WebKitUserContentManager *content_manager;
+  WebKitWebView *web_view;
+  FlMethodChannel *method_channel;
+  FlEventChannel *event_channel;
   gboolean event_listening;
-  GHashTable* pending_nav_decisions;
-  GHashTable* pending_auth_requests;
-  GHashTable* pending_permission_requests;
-  GHashTable* pending_script_dialogs;
-  GHashTable* pending_tls_errors;
-  GHashTable* js_channel_signal_ids;
-  GHashTable* js_channels;
+  GHashTable *pending_nav_decisions;
+  GHashTable *pending_auth_requests;
+  GHashTable *pending_permission_requests;
+  GHashTable *pending_script_dialogs;
+  GHashTable *pending_tls_errors;
+  GHashTable *js_channel_signal_ids;
+  GHashTable *js_channels;
   gint next_request_id;
   gboolean console_enabled;
   gboolean scroll_enabled;
+  gboolean java_script_alert_dialog_enabled;
+  gboolean java_script_confirm_dialog_enabled;
+  gboolean java_script_prompt_dialog_enabled;
+  gboolean vertical_scrollbar_enabled;
+  gboolean horizontal_scrollbar_enabled;
+  const gchar *over_scroll_behavior;
   gint frame_x;
   gint frame_y;
   gint frame_width;
@@ -45,28 +51,27 @@ typedef struct {
 } LinuxWebView;
 
 typedef struct {
-  LinuxWebView* webview;
-  gchar* name;
+  LinuxWebView *webview;
+  gchar *name;
 } JavaScriptChannelHandlerData;
 
 typedef struct {
-  GTlsCertificate* certificate;
-  gchar* host;
-  gchar* uri;
+  GTlsCertificate *certificate;
+  gchar *host;
+  gchar *uri;
 } PendingTlsError;
 
 typedef struct {
-  WebKitPolicyDecision* decision;
-  gchar* uri;
+  WebKitPolicyDecision *decision;
+  gchar *uri;
   gboolean open_in_place;
 } PendingNavigationDecision;
 
-LinuxWebView* create_linux_webview(WebviewAllLinuxPlugin* self);
+LinuxWebView *create_linux_webview(WebviewAllLinuxPlugin *self);
 void destroy_linux_webview(gpointer data);
-void root_method_call_cb(FlMethodChannel* channel,
-                         FlMethodCall* method_call,
+void root_method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
                          gpointer user_data);
-GtkOverlay* ensure_overlay(WebviewAllLinuxPlugin* self);
-void update_flutter_view_input_region(WebviewAllLinuxPlugin* self);
+GtkOverlay *ensure_overlay(WebviewAllLinuxPlugin *self);
+void update_flutter_view_input_region(WebviewAllLinuxPlugin *self);
 
-#endif  // WEBVIEW_ALL_LINUX_PLUGIN_PRIVATE_H_
+#endif // WEBVIEW_ALL_LINUX_PLUGIN_PRIVATE_H_
