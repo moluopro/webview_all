c8681ba11e576886c04498a2f64e0d852faa7bb7
moluopro <moluopro@qq.com>
2026-06-30 12:45:34 +0800

    feat(linux): expand WebKit internal state

--- webview_all_linux/linux/src/webview/webview_internal.h
+++ webview_all_linux/linux/src/webview/webview_internal.h
@@ -3,29 +3,28 @@
 
 #include "plugin/webview_all_linux_plugin_private.h"
 
-void send_event(LinuxWebView* webview, FlValue* event);
-void update_history(LinuxWebView* webview);
-void emit_url_change(LinuxWebView* webview);
-void emit_title_change(LinuxWebView* webview);
-void emit_load_error(LinuxWebView* webview,
-                     GError* error,
-                     const gchar* failing_url);
-void evaluate_javascript(WebKitWebView* web_view,
-                         const gchar* script,
-                         FlMethodCall* method_call);
-void rebuild_user_scripts(LinuxWebView* webview);
-void console_message_received_cb(WebKitUserContentManager* manager,
-                                 WebKitJavascriptResult* result,
+void send_event(LinuxWebView *webview, FlValue *event);
+void update_history(LinuxWebView *webview);
+void emit_url_change(LinuxWebView *webview);
+void emit_title_change(LinuxWebView *webview);
+void emit_load_error(LinuxWebView *webview, GError *error,
+                     const gchar *failing_url);
+void evaluate_javascript(WebKitWebView *web_view, const gchar *script,
+                         FlMethodCall *method_call);
+gchar *build_scrollbar_style_script(LinuxWebView *webview);
+gchar *build_overscroll_style_script(LinuxWebView *webview);
+void rebuild_user_scripts(LinuxWebView *webview);
+void console_message_received_cb(WebKitUserContentManager *manager,
+                                 WebKitJavascriptResult *result,
                                  gpointer user_data);
-void scroll_message_received_cb(WebKitUserContentManager* manager,
-                                WebKitJavascriptResult* result,
+void scroll_message_received_cb(WebKitUserContentManager *manager,
+                                WebKitJavascriptResult *result,
                                 gpointer user_data);
-void javascript_channel_message_received_cb(WebKitUserContentManager* manager,
-                                            WebKitJavascriptResult* result,
+void javascript_channel_message_received_cb(WebKitUserContentManager *manager,
+                                            WebKitJavascriptResult *result,
                                             gpointer user_data);
-void destroy_js_channel_handler_data(gpointer data, GClosure* closure);
-void instance_method_call_cb(FlMethodChannel* channel,
-                             FlMethodCall* method_call,
-                             gpointer user_data);
+void destroy_js_channel_handler_data(gpointer data, GClosure *closure);
+void instance_method_call_cb(FlMethodChannel *channel,
+                             FlMethodCall *method_call, gpointer user_data);
 
-#endif  // WEBVIEW_ALL_LINUX_WEBVIEW_INTERNAL_H_
+#endif // WEBVIEW_ALL_LINUX_WEBVIEW_INTERNAL_H_
